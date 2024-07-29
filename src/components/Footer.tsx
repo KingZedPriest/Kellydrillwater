6bfb1ea1f1ff919914452b488e0e725a5006b0f0
Priest <Charleschukwuemeka47@gmail.com>
2024-07-29 20:53:08 +0100

    Fix: Edit the header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,61 +8,61 @@ import logo from "../../public/mainLogo.png";
 //Deafult Mechanism
 import Image from "next/image";
 import Link from "next/link";
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <main className="py-10 px-6 sm:px-12 lg:px-24 md:px-18 bg-secAccentColor text-white">
             <div className="text-white flex flex-col space-y-8 lg:space-y-0 lg:flex-row justify-between">
-            <div className="flex flex-col">
-                <h4 className="font-bold">Locations</h4>
-                <p className="mt-2">Anambra</p>
-                <p className="mt-2">Enugu</p>
-                <p className="mt-2">Ebonyi</p>
-                <p className="mt-2">Delta</p>
-                <p className="mt-2">Abia</p>
-                <p className="mt-2">Imo</p>
-            </div>
-            <div className="flex flex-col">
-            <h4 className="font-bold">About Us</h4>
-            <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Our Drillers</Link>
-            <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Why Us?</Link>
-            <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Services</Link>
-            <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Project Tools</Link>
-            <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Partner With Us</Link>
-            </div>
-            <div className="flex flex-col">
-            <h4 className="font-bold">Contact Us</h4>
-            <p className="mt-2">Email: contactus@Kellydrillwater.com</p>
-            <p className="mt-2">Call: +234 81 6024 4412</p>
-            <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">FAQ</Link>         
-            </div>
-            <div className="flex flex-col">
-            <h4 className="font-bold">Newsletter</h4>
-                <p className="mt-2 capitalize">Join our community and be the first to know of any update.</p>
-                <div className="mt-8 flex flex-row items-center gap-x-1">
-                    <input className="w-56 sm:w-60 py-3 px-4 focus:outline-headersColor text-black caret-headersColor placeholder:text-sm rounded-md" type="email" name="email" id="email" placeholder="Enter Your Email..." />
-                    <button className="font-semibold md:w-40 bg-headersColor border-r-4 border-headersColor px-2 py-3 rounded-md hover:bg-white hover:text-containerColor duration-500" ><Link href="/">Subscribe</Link></button>
+                <div className="flex flex-col">
+                    <h4 className="font-bold">Locations</h4>
+                    <p className="mt-2">Anambra</p>
+                    <p className="mt-2">Enugu</p>
+                    <p className="mt-2">Ebonyi</p>
+                    <p className="mt-2">Delta</p>
+                    <p className="mt-2">Abia</p>
+                    <p className="mt-2">Imo</p>
                 </div>
-            </div>
-        </div>
-        <div className="mt-16 flex flex-row justify-between border-y border-white py-4">
-            <div className="flex flex-row items-center">
-                <div className="border-r border-white pr-4">
-                    <div className="p-4 bg-white">
-                        <Image src={logo} alt="Kellydrill Logo" className="w-32 md:w-10"/>  
+                <div className="flex flex-col">
+                    <h4 className="font-bold">About Us</h4>
+                    <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Our Drillers</Link>
+                    <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Why Us?</Link>
+                    <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Services</Link>
+                    <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Project Tools</Link>
+                    <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">Partner With Us</Link>
+                </div>
+                <div className="flex flex-col">
+                    <h4 className="font-bold">Contact Us</h4>
+                    <p className="mt-2">Email: contactus@Kellydrillwater.com</p>
+                    <p className="mt-2">Call: +234 81 6024 4412</p>
+                    <Link className="mt-2 hover:bg-headersColor duration-500 hover:px-2 rounded-md py-2" href="/">FAQ</Link>
+                </div>
+                <div className="flex flex-col">
+                    <h4 className="font-bold">Newsletter</h4>
+                    <p className="mt-2 capitalize">Join our community and be the first to know of any update.</p>
+                    <div className="mt-8 flex flex-row items-center gap-x-1">
+                        <input className="w-56 sm:w-60 py-3 px-4 focus:outline-headersColor text-black caret-headersColor placeholder:text-sm rounded-md" type="email" name="email" id="email" placeholder="Enter Your Email..." />
+                        <button className="font-semibold md:w-40 bg-headersColor border-r-4 border-headersColor px-2 py-3 rounded-md hover:bg-white hover:text-containerColor duration-500" ><Link href="/">Subscribe</Link></button>
                     </div>
                 </div>
-               <div className="ml-4">
-               <p className="headingFont text-white text-xs md:text-sm lg:text-base">Best Drilling Company In Nigeria.</p>
-               </div>
             </div>
-            <div className="flex flex-row space-x-2 md:space-x-8 my-auto">
-            <Link href="https://web.facebook.com/profile.php?id=100076353184277" target="_blank" rel="noopener noreferrer"><BsFacebook className="cursor-pointer" size={20}/></Link>
-            <Link href=""><AiFillInstagram className="cursor-pointer" size={20}/></Link>
-            <Link href=""><BsTwitter className="cursor-pointer" size={20}/></Link>
-            <Link href=""><AiFillLinkedin className="cursor-pointer" size={20}/></Link>
+            <div className="mt-16 flex flex-row justify-between border-y border-white py-4">
+                <div className="flex flex-row items-center">
+                    <div className="border-r border-white pr-4">
+                        <div className="p-4 bg-white">
+                            <Image src={logo} alt="Kellydrill Logo" className="w-20 md:w-10" />
+                        </div>
+                    </div>
+                    <div className="ml-4">
+                        <p className="headingFont text-white text-xs md:text-sm lg:text-base">Best Drilling Company In Nigeria.</p>
+                    </div>
+                </div>
+                <div className="flex flex-row space-x-2 md:space-x-8 my-auto">
+                    <Link href="https://web.facebook.com/profile.php?id=100076353184277" target="_blank" rel="noopener noreferrer"><BsFacebook className="cursor-pointer" size={20} /></Link>
+                    <Link href=""><AiFillInstagram className="cursor-pointer" size={20} /></Link>
+                    <Link href=""><BsTwitter className="cursor-pointer" size={20} /></Link>
+                    <Link href=""><AiFillLinkedin className="cursor-pointer" size={20} /></Link>
+                </div>
+
             </div>
-            
-        </div>
         </main>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,44 +13,32 @@ import logo from "../../public/mainLogo.png";
 import Image from "next/image";
 
 export default function Header() {
-  // For the Hamburger Menu
+
+  // State for Hamburger Menu
   const [visible, setVisible] = useState<boolean>(false);
+
+  // Visibility function
   const setVisibility = () => {
     setVisible(!visible);
   };
+
   return (
     <main className="relative">
       <div className="md:px-18 flex items-center justify-between px-6 py-10 sm:px-12 lg:px-24">
         <Link href="/">
-          <Image
-            src={logo}
-            alt="Kelly Drill Company Logo"
-            className="w-10 cursor-pointer duration-500 hover:grayscale"
-          />
+          <Image src={logo} alt="Kelly Drill Company Logo" className="w-10 cursor-pointer duration-500 hover:grayscale" />
         </Link>
         <div className="hidden gap-5 md:flex">
-          <Link
-            className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white"
-            href="/"
-          >
+          <Link className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white" href="/">
             Home
           </Link>
-          <Link
-            className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white"
-            href="/services"
-          >
+          <Link className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white" href="/services">
             Services
           </Link>
-          <Link
-            className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white"
-            href="/about"
-          >
+          <Link className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white" href="/about">
             About Us
           </Link>
-          <Link
-            className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white"
-            href="/contact"
-          >
+          <Link className="hover:bg-secAccentColor rounded-lg px-3 py-2 font-semibold duration-500 hover:text-white" href="/contact">
             Contact Us
           </Link>
         </div>
@@ -59,12 +47,9 @@ export default function Header() {
             className="text-green-600 duration-500 hover:-translate-y-2"
             href="https://api.whatsapp.com/send?phone=%2B2348160244412&data=ARB3st80c0EmKqQPQM23W3wsTdIKITer1B8PXqpvBR2WKihPl7t3B_N03p0WwQ-l6mnWTAJDnKGM0Vo6wM9re9f82KivFkwu23AO-VwDsjoAHFUTIFh5jsTtukDd0nPhYoL-oxRwnwvhmxkDoKaxbIwmEA&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR1U0dhQUKjqpldb4yxJHYyBLiUJSq1SgDUVnDI5kIxAg9ZEeMWyF1IpCYk"
             target="_blank" rel="noopener noreferrer">
-            <Whatsapp size="40" variant="Broken"/>
+            <Whatsapp size="40" variant="Broken" />
           </Link>
-          <Link
-            className="bg-headersColor hover:bg-secAccentColor rounded-xl px-8 py-3 font-semibold text-white duration-500 hover:-translate-y-2"
-            href="tel:+2348160244412"
-          >
+          <Link className="bg-headersColor hover:bg-secAccentColor rounded-xl px-8 py-3 font-semibold text-white duration-500 hover:-translate-y-2" href="tel:+2348160244412">
             Call Now
           </Link>
         </div>
@@ -73,49 +58,34 @@ export default function Header() {
         </div>
       </div>
       {/* Collapsed Nav bar */}
-      <div className={`${ visible ? "menu" : "hidden"} bg-accentColor fixed right-0 top-0 z-10 h-screen w-80 md:hidden`}>
-          <CloseSquare
-            size={30}
-            className="cursor-pointer text-white ml-6 mt-10 "
-            onClick={setVisibility}
-          />
+      <div className={`${visible ? "translate-x-0" : "translate-x-full duration-300"} bg-accentColor fixed right-0 top-0 z-10 h-screen w-72 md:hidden duration-300`}>
+        <CloseSquare
+          size={30}
+          className="cursor-pointer text-white ml-6 mt-10 "
+          onClick={setVisibility}
+        />
         <div className="mt-10 flex flex-col gap-5 px-4">
-          <Link
-            className=" hover:bg-secAccentColor rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white"
-            href="/"
-          >
+          <Link onClick={setVisibility} className=" hover:bg-secAccentColor rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white" href="/">
             Home
           </Link>
-          <Link
-            className=" hover:bg-secAccentColor hover: rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white"
-            href="/services"
-          >
+          <Link onClick={setVisibility} className=" hover:bg-secAccentColor hover: rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white" href="/services">
             Services
           </Link>
-          <Link
-            className=" hover:bg-secAccentColor rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white"
-            href="/about"
-          >
+          <Link onClick={setVisibility} className=" hover:bg-secAccentColor rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white" href="/about">
             About Us
           </Link>
-          <Link
-            className=" hover:bg-secAccentColor rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white"
-            href="/contact"
-          >
+          <Link onClick={setVisibility} className=" hover:bg-secAccentColor rounded-lg ml-3 hover:px-3 py-2 font-semibold text-white duration-500 hover:border-r-4 hover:border-white" href="/contact">
             Contact Us
           </Link>
           <hr />
-          <div className="mt-2 flex items-center gap-5">
+          <div onClick={setVisibility} className="mt-2 flex items-center gap-5">
             <Link
               className=" text-white duration-500 hover:-translate-y-2"
               href="https://api.whatsapp.com/send?phone=%2B2348160244412&data=ARB3st80c0EmKqQPQM23W3wsTdIKITer1B8PXqpvBR2WKihPl7t3B_N03p0WwQ-l6mnWTAJDnKGM0Vo6wM9re9f82KivFkwu23AO-VwDsjoAHFUTIFh5jsTtukDd0nPhYoL-oxRwnwvhmxkDoKaxbIwmEA&source=FB_Page&app=facebook&entry_point=page_cta&fbclid=IwAR1U0dhQUKjqpldb4yxJHYyBLiUJSq1SgDUVnDI5kIxAg9ZEeMWyF1IpCYk"
             >
-              <Whatsapp size="36" variant="Bold"/>
+              <Whatsapp size="36" variant="Bold" />
             </Link>
-            <Link
-              className=" rounded-xl border-2 border-white px-8 py-2 font-semibold text-white duration-500 hover:-translate-y-2"
-              href="tel:+2348160244412"
-            >
+            <Link onClick={setVisibility} className=" rounded-xl border-2 border-white px-8 py-2 font-semibold text-white duration-500 hover:-translate-y-2" href="tel:+2348160244412">
               Call Now
             </Link>
           </div>
