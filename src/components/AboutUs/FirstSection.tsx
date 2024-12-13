f22d86864c8777045cb476c1509c12e070f8472b
Priest <Charleschukwuemeka47@gmail.com>
2024-12-13 01:15:16 +0100

    Feat: Finish the first section

--- src/components/AboutUs/FirstSection.tsx
+++ src/components/AboutUs/FirstSection.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 //Images
 import about from "../../../public/about.jpg";
 import about1 from "../../../public/about1.jpg";
 import about2 from "../../../public/about2.jpg";
+
+//Icons
+import { CallReceived } from "iconsax-react";
 
 const FirstSection = () => {
     return (
@@ -38,6 +42,13 @@ const FirstSection = () => {
                 <div className="flex flex-col gap-y-10 lg:mt-10 lg:w-[45%] xl:w-[48%]">
                     <p>At Kellydrill, our journey began with a simple yet profound curiosity: <i className="text-linkColor">How does water emerge from the depths of the earth? </i> This passion ignited over a decade ago and has since grown into a mission to provide reliable, sustainable water solutions to communities and industries alike. From residential neighborhoods to sprawling commercial estates and industrial facilities, we’ve dedicated ourselves to ensuring access to clean and dependable water sources.</p>
                     <p>Headquartered at No. 16 Ukwuorji Junction, Awka, Anambra State, with a branch located at Nkwelle Ezunanka, behind Goldland Hotel, Kellydrill has become a trusted name in water drilling and management. Our team of experienced drilling experts and hydrologists employs cutting-edge technology to locate and harness underground water with precision. As a duly registered company (RC: 3778486), we take pride in delivering high-quality water infrastructure, tailored to meet the unique needs of our diverse clientele.</p>
+                    <div className="flex gap-x-5 lg:mt-10 p-2 md:p-3 xl:p-4 border border-black/60 rounded-2xl w-fit">
+                        <Link href="tel:+2348160244412"><CallReceived size={32} className="text-linkColor" variant="Bold"/></Link>
+                        <div>
+                            <p className="font-bold text-base md:text-lg xl:text-xl">+234 81 6024 4412</p>
+                            <p className="mt-1 text-[10px] xl:text-sm md:text-xs">Call For Booking</p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
